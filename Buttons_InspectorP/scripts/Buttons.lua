0453524df4e4d0e144fe96be9dc80eec7fc23c76
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:38:13 +0200

    'Update'

--- Buttons_InspectorP/scripts/Buttons.lua
+++ Buttons_InspectorP/scripts/Buttons.lua
@@ -1,22 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  Buttons_InspectorP
-
-  Summary:
-  Using the buttons on InspectorP devices.
-
-  How to Run:
-  A connected InpspectorP device is necessary to run this sample.
-  This script using the button events 'OnPressed' and 'OnReleased' from two buttons
-  of the InspectorP. Each event will call the according function. Starting this sample
-  is possible either by running the app (F5) or debugging (F7+F10). When it is running,
-  the results can be seen when pressing the buttons on the device.
-
-  More information can be found in the Tutorial
-  "Audio-Visual Feedback InspectorP"
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
